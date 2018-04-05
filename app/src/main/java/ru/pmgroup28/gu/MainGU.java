8bef562bf43d712f2ffaf6c45a0094f58af71428
Alexander Pavlov <pavlov15046@gmail.com>
2018-04-05 22:25:25 +0900

    Создано приложение РПГУ

---
+++ app/src/main/java/ru/pmgroup28/gu/MainGU.java
@@ -0,0 +1,13 @@
+package ru.pmgroup28.gu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainGU extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_gu);
+    }
+}
